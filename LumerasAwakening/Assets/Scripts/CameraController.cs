5c54133d8bfcaef1951aefc0603461dad30f9a04
Sola <solanylander@gmail.com>
2017-01-28 15:12:54 -0500

    Basic test room

---
+++ LumerasAwakening/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    private float distance;
+    private float angle;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+        angle = (float) Math.Atan(offset.z/offset.x);
+        distance = (float) Math.Sqrt(offset.z*offset.z +offset.x*offset.x);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float pan = Input.GetAxis("CameraPan");
+        angle += pan * 0.01f;
+        if(angle > Math.PI)
+        {
+            angle -= (float) Math.PI;
+        }else if(angle < 0.0f)
+        {
+            angle += (float)Math.PI;
+        }
+        Debug.Log(angle);
+        offset = new Vector3((float) Math.Cos(angle) / distance, offset.y, (float)Math.Sin(angle) / distance);
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ LumerasAwakening/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpHeight;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed *= 0.1f;
+    }
+	
+	// Update is called once per fixed increment
+	void FixedUpdate () {
+        float moveHorizontal = -Input.GetAxis("Horizontal");
+        float moveVertical = -Input.GetAxis("Vertical");
+        bool jumpPressed = Input.GetButtonDown("Jump");
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+        transform.position += movement;
+
+        if (jumpPressed)
+        {
+            Vector3 jump = new Vector3(0.0f, jumpHeight * 10, 0.0f);
+            rb.AddForce(jump);
+        }
+    }
+}
