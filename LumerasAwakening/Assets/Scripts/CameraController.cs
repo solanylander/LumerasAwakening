b15f57e7694f9a32e072d95dd249ec397e68fd13
Sola <solanylander@gmail.com>
2017-01-28 15:40:56 -0500

    Camera now pans around the player using Q and E

--- LumerasAwakening/Assets/Scripts/CameraController.cs
+++ LumerasAwakening/Assets/Scripts/CameraController.cs
@@ -10,14 +10,13 @@ public class CameraController : MonoBehaviour
     private Vector3 offset;
     private float distance;
     private float angle;
-
     // Use this for initialization
     void Start()
     {
         offset = transform.position - player.transform.position;
-
         angle = (float) Math.Atan(offset.z/offset.x);
         distance = (float) Math.Sqrt(offset.z*offset.z +offset.x*offset.x);
+        transform.Rotate(0.0f, 180.0f, 0.0f, Space.World);
     }
 
     // Update is called once per frame
@@ -25,15 +24,16 @@ public class CameraController : MonoBehaviour
     {
         float pan = Input.GetAxis("CameraPan");
         angle += pan * 0.01f;
-        if(angle > Math.PI)
+        if (angle > Math.PI)
         {
-            angle -= (float) Math.PI;
-        }else if(angle < 0.0f)
-        {
-            angle += (float)Math.PI;
+            angle -= 2.0f * (float)Math.PI;
         }
-        Debug.Log(angle);
-        offset = new Vector3((float) Math.Cos(angle) / distance, offset.y, (float)Math.Sin(angle) / distance);
+        else if (angle < -Math.PI)
+        {
+            angle += 2.0f * (float)Math.PI;
+        }
+        offset = new Vector3((float)Math.Cos(angle) * distance, offset.y, (float)Math.Sin(angle) * distance);
         transform.position = player.transform.position + offset;
+        transform.Rotate(0.0f, -360.0f * (pan * 0.01f / (2.0f * (float)Math.PI)), 0.0f, Space.World);
     }
 }
