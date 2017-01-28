619a9f673a8a711b7d26acb2914a2b6b2c41bc85
Sola <solanylander@gmail.com>
2017-01-28 17:17:50 -0500

    Player moves with respect to the camera

--- LumerasAwakening/Assets/Scripts/CameraController.cs
+++ LumerasAwakening/Assets/Scripts/CameraController.cs
@@ -10,19 +10,28 @@ public class CameraController : MonoBehaviour
     private Vector3 offset;
     private float distance;
     private float angle;
+    public float panSpeed;
     // Use this for initialization
     void Start()
     {
+        //Determines the distance between the player and the camera
         offset = transform.position - player.transform.position;
+        //Angle of the camera with respect to vertices x,z to the player
         angle = (float) Math.Atan(offset.z/offset.x);
+        //Distance between the player and the camera with respect to vertices x,z
         distance = (float) Math.Sqrt(offset.z*offset.z +offset.x*offset.x);
+        //This is needed to initially face the correct direction
         transform.Rotate(0.0f, 180.0f, 0.0f, Space.World);
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    // FixedUpdate is called once per fixed increment
+    void FixedUpdate()
     {
-        float pan = Input.GetAxis("CameraPan");
+
+        //Get input
+        float pan = -Input.GetAxis("CameraPan") * panSpeed;
+
+        //Recalculate the cameras angle to the player
         angle += pan * 0.01f;
         if (angle > Math.PI)
         {
@@ -32,8 +41,11 @@ public class CameraController : MonoBehaviour
         {
             angle += 2.0f * (float)Math.PI;
         }
+
+        //Update the offset after panning
         offset = new Vector3((float)Math.Cos(angle) * distance, offset.y, (float)Math.Sin(angle) * distance);
         transform.position = player.transform.position + offset;
+        //Rotate camera to still face the player object
         transform.Rotate(0.0f, -360.0f * (pan * 0.01f / (2.0f * (float)Math.PI)), 0.0f, Space.World);
     }
 }
--- LumerasAwakening/Assets/Scripts/PlayerController.cs
+++ LumerasAwakening/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
+    public new GameObject camera;
+    private Vector3 offset;
     public float speed;
     public float jumpHeight;
     private Rigidbody rb;
+    private float angle;
+    private bool canJump;
+    public float panSpeed;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
+        //Determines the distance between the player and the camera
+        offset = camera.transform.position - transform.position;
+        //Angle of the camera with respect to vertices x,z to the player
+        angle = (float)Math.Atan(offset.z / offset.x);
         rb = GetComponent<Rigidbody>();
+        //Possible to jump
+        canJump = true;
         speed *= 0.1f;
     }
-	
-	// Update is called once per fixed increment
-	void FixedUpdate () {
+
+    // FixedUpdate is called once per fixed increment
+    void FixedUpdate()
+    {
+        //Get inputs
         float moveHorizontal = -Input.GetAxis("Horizontal");
         float moveVertical = -Input.GetAxis("Vertical");
         bool jumpPressed = Input.GetButtonDown("Jump");
-        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
-        transform.position += movement;
+        float pan = -Input.GetAxis("CameraPan") * panSpeed;
 
-        if (jumpPressed)
+        //If it is possible to jump then jump
+        if (jumpPressed && canJump)
         {
             Vector3 jump = new Vector3(0.0f, jumpHeight * 10, 0.0f);
             rb.AddForce(jump);
+        }
+
+        //Recalculate the cameras angle to the player
+        angle += pan * 0.01f;
+        if (angle > Math.PI)
+        {
+            angle -= 2.0f * (float)Math.PI;
+        }
+        else if (angle < -Math.PI)
+        {
+            angle += 2.0f * (float)Math.PI;
+        }
+        
+        //Convert x,z axes in axes based on the direction of the camera
+        float horizontal = (float) Math.Cos(angle) * moveVertical + (float)Math.Sin(angle) * moveHorizontal;
+        float vertical = (float)Math.Sin(angle) * moveVertical - (float)Math.Cos(angle) * moveHorizontal;
+        //translate the player
+        Vector3 movement = new Vector3(horizontal * speed, 0.0f, vertical * speed);
+        transform.position += movement;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //If you are on the floor you can jump
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            canJump = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //If you are not on the floor you can not jump
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            canJump = false;
         }
     }
 }
