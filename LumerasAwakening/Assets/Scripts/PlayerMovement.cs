887e38340dd3e18c9829a93124c492505440df13
John Lacuna <john.lacuna@mail.utoronto.ca>
2017-02-02 15:00:42 -0500

    Player movement and choosing objects

--- LumerasAwakening/Assets/Scripts/PlayerMovement.cs
+++ LumerasAwakening/Assets/Scripts/PlayerMovement.cs
@@ -21,17 +21,21 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		
-        // Slow down time on button press
+        // Slow down time on button press (Left CTRL)
         if (Input.GetButton("Fire1")) {
             Time.timeScale = 0.2F;
             findObjectsInRadius();
+
+            // If there are objects in range
             if(inRange.Count != 0) {
                 if (i >= inRange.Count)
                         i = 0;
                 currObject = inRange[i];
                 rend = currObject.GetComponent<Renderer>();
                 rend.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
-                if(Input.GetButtonDown("Fire3")) {
+
+                // Switch between objects (Left SHIFT)
+                if (Input.GetButtonDown("Fire3")) {
                     i++;
                     rend.material.shader = Shader.Find("Diffuse");
                     if (i >= inRange.Count)
@@ -41,6 +45,8 @@ public class PlayerMovement : MonoBehaviour {
                     rend = currObject.GetComponent<Renderer>();
                     rend.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
                 }
+
+                // Perform action on chosen object (Left ALT)
                 if (Input.GetButtonDown("Fire2")) {
                     Debug.Log(currObject);
                     currObject.SetActive(false);
@@ -48,6 +54,7 @@ public class PlayerMovement : MonoBehaviour {
             }
         }
 
+        // Regular time speed
         else 
             Time.timeScale = 1.0F;
         Time.fixedDeltaTime = 0.02F * Time.timeScale;
@@ -55,22 +62,26 @@ public class PlayerMovement : MonoBehaviour {
         this.transform.rotation = Quaternion.Euler(0, 0, 0);
 	}
 
+    // Mechanic for preventing jumping mid-air
     void OnCollisionStay(Collision collision) {
-
-        if(collision.gameObject.tag == "ground" && Input.GetButtonDown("Jump")) {
-            rb.AddForce (0, 300, 0);
+        if((collision.gameObject.tag == "ground" || collision.gameObject.tag == "adjustable") && Input.GetButtonDown("Jump")) {
+            rb.AddForce (0, 600, 0);
         }
     }
 
+    // Puts objects in range in a list for use
     void findObjectsInRadius() {
         foreach (GameObject ob in adjustables) {
             float distanceSqr = (transform.position - ob.transform.position).sqrMagnitude;
+
+            // Object in range
             if (distanceSqr < 20) {
                 if (!inRange.Contains(ob)) {
                     inRange.Add(ob);
                 }
-                //ob.SetActive(false);
             }
+
+            // Object not in range
             else {
                 inRange.Remove(ob);
                 ob.SetActive(true);
