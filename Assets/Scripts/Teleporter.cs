d3ca139e5bac5120f6959319c8d406daec2b7cab
Ben-Hu <benjqh@gmail.com>
2017-03-11 01:35:47 -0500

    teleporters

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+	public Transform teleporterEnd;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Contains("Player") && teleporterEnd != null)
+        {
+            col.gameObject.transform.position = teleporterEnd.position;
+            //play teleport sound
+        }
+    }
+}
