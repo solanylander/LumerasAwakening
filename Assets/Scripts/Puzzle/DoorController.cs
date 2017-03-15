5522fa0bb748e329fe48d2acbdd71410c54b50ab
Sola <solanylander@gmail.com>
2017-03-15 02:51:13 -0400

    Worked on tutorial and changed some stuff in gears path

--- Assets/Scripts/Puzzle/DoorController.cs
+++ Assets/Scripts/Puzzle/DoorController.cs
@@ -15,13 +15,17 @@ public class DoorController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-		if(open && door.transform.localScale.y > Size && !grow)
+	void FixedUpdate ()
+    {
+        if (open && door.transform.localScale.y > Size && !grow)
         {
             door.transform.localScale = new Vector3(door.transform.localScale.x, door.transform.localScale.y - 0.05f, door.transform.localScale.z);
         }else if(open && door.transform.localScale.z < Size && grow)
         {
             door.transform.localScale = new Vector3(door.transform.localScale.x, door.transform.localScale.y, door.transform.localScale.z + 0.05f);
+        }else if((open && door.transform.localScale.z >= Size && grow) || (open && door.transform.localScale.y <= Size && !grow))
+        {
+            Destroy(gameObject);
         }
 	}
 
