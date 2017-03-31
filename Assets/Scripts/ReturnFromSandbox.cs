5cbd3f78747bf957f6e56baa0ec5b290edea63fe
Ben-Hu <benjqh@gmail.com>
2017-03-31 17:29:57 -0400

    add return to main level from funzone

---
+++ Assets/Scripts/ReturnFromSandbox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnFromSandbox : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Contains("Player"))
+        {
+            //Other Stuff
+            SceneManager.LoadScene("Scenes/the-end");
+        }
+    }
+}
