b0a7dd978a0233ee0eab0de8ff122badb252c694
Ben-Hu <benjqh@gmail.com>
2017-03-31 17:21:00 -0400

    disable scripts in menu scene, simple rotation

---
+++ Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour {
+    public float rotateSpeed = 5;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion newRot = Quaternion.Euler(0, transform.eulerAngles.y + 0.025f, 0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRot, 1f);
+	}
+}
