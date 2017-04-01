933674fe28c9931c01e7cb5b1f577ad16a9f4b0d
Ben-Hu <benjqh@gmail.com>
2017-04-01 18:39:25 -0400

    catch rocks

---
+++ Assets/Scripts/CapturedRock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturedRock : MonoBehaviour
+{
+    public GameObject triggerObject;
+
+    void Update()
+    {
+        if (!triggerObject.activeSelf)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+            for (int i = 0; i < transform.childCount; ++i)
+            {
+                transform.GetChild(i).gameObject.SetActiveRecursively(true);
+            }
+        }
+    }
+}
