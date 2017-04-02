313e1ba0592be74a40190cef8c32f4dfc0139937
Ben-Hu <benjqh@gmail.com>
2017-04-02 01:27:22 -0400

    goal signposting

---
+++ Assets/Scripts/PopupTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupTrigger : MonoBehaviour {
+    public GameObject popup;
+    private bool spent;
+
+	// Use this for initialization
+	void Start () {
+        spent = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag.Contains("Player") && !spent)
+        {
+            spent = true;
+            popup.SetActive(true);
+        }
+    }
+}
